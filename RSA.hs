6db8f04a03e1746d3727d65dbc3bd0ffa540f592
Chregi Glatthard <chregi.glatthard@gmail.com>
2013-04-17 13:33:39 +0200

    scaffolding

---
+++ RSA.hs
@@ -0,0 +1,24 @@
+-- Define RSA module and its public functions
+module RSA
+(  generatePrivateKey
+,  generatePublicKey
+,  encrypt
+,  decrypt
+) where
+
+-- import required external modules
+import Prelude 		-- contains gcd function(greatest common decimal)
+
+-- private functions
+
+-- get Tupel of two large primes
+--getPrimes
+
+-- 
+--getSmallOddInt
+
+-- public functions
+--generatePrivateKey
+--generatePublicKey
+--encrypt 
+--decrypt
