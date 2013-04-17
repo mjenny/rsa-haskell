3d0bd9a6d4067f2c906763ba0849f3c086b3d726
Chregi Glatthard <chregi.glatthard@gmail.com>
2013-04-17 14:15:16 +0200

    more scaffolding

--- RSA.hs
+++ RSA.hs
@@ -1,4 +1,4 @@
--- Define RSA module and its public functions
+-- # Define RSA module and its public functions
 module RSA
 (  generatePrivateKey
 ,  generatePublicKey
@@ -6,13 +6,22 @@ module RSA
 ,  decrypt
 ) where
 
--- import required external modules
+-- # import required external modules
 import Prelude 		-- contains gcd function(greatest common decimal)
 
--- private functions
+-- # private functions
 
--- get Tupel of two large primes
+-- # get Tupel of two large primes
 --getPrimes
+{-| 
+	* this is a multi-line comment *
+	
+	Possibility to generate large prime numbers:
+	1. create candidate: odd number n of appropriate size
+	2. test n for primality (z.B. Miller-Rabin?)
+	3. if n is no prime, return to step 1
+
+-}
 
 -- 
 --getSmallOddInt
