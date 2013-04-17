36ce959c9627a0ef206562fc89482dac6b81fb58
Chregi Glatthard <chregi.glatthard@gmail.com>
2013-04-17 18:02:33 +0200

    add prime functions from stackoverflow

--- RSA.hs
+++ RSA.hs
@@ -8,6 +8,10 @@ module RSA
 
 -- # import required external modules
 import Prelude 		-- contains gcd function(greatest common decimal)
+import System.Random
+import Control.Monad.Fix 
+import Data.Bits
+
 
 -- # private functions
 
@@ -23,12 +27,32 @@ import Prelude 		-- contains gcd function(greatest common decimal)
 
 -}
 
--- 
---getSmallOddInt
+-- Source: http://stackoverflow.com/questions/13300533/haskell-prime-number-generator-according-to-bits-for-very-large-numbers
+
+rndPrime :: Int -> IO Integer
+rndPrime bits =
+    fix $ \again -> do
+        x <- fmap (.|. 1) $ randomRIO (2^(bits - 1), 2^bits - 1)
+        if millerRabin x then return x else again
+
+rndPrimes :: Int -> IO (Integer, Integer)
+rndPrimes bits = do
+    p <- rndPrime bits
+    fix $ \again -> do
+        q <- rndPrime bits
+        if p /= q then return (p, q) else again
+
+millerRabin :: 
+millerRabin x = (mod (2^(x-1)) x) not (1)
+
+
+-- use to save performance while doing millerRabin
+-- shiftR = bitwise right shift
+--squareMultiply b e = 
 
 -- public functions
 --generatePrivateKey
 --generatePublicKey
-encrypt k n M = mod (M^k) n
+encrypt k n m = mod (m^k) n
 
-decrypt d n C = mod (C^d) n
+decrypt d n c = mod (c^d) n
