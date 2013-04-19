a88e14eadde19c02fac53bfa0cd75a6a8a31fd1c
Manuel Jenny <manuel.jenny@hispeed.ch>
2013-04-19 15:05:52 +0200

    added extended euclidean algorithm and inverse modulo function

--- RSA.hs
+++ RSA.hs
@@ -1,7 +1,6 @@
 -- # Define RSA module and its public functions
 module RSA
-(  generatePrivateKey
-,  generatePublicKey
+(  generateKeyPair
 ,  encrypt
 ,  decrypt
 ) where
@@ -66,14 +65,46 @@ isNotDivisor :: [Integer] -> Integer -> Bool
 isNotDivisor [] _ = True
 isNotDivisor (w:ws) x | (mod x w) == 0 = False
 		      | otherwise = isNotDivisor ws x
+		      
+-- http://blog.agrafix.net/2012/08/haskell-multiplikatives-inverse-in-einem-restklassenring/	
+-- multiplicative inverse for a in Z \ bZ
+inverseMod :: Integer -> Integer -> Integer
+inverseMod a m =
+  (x + m) `mod` m
+  where
+    (gcd, (x, y)) = euclid a m
+
+-- http://blog.agrafix.net/2012/08/haskell-multiplikatives-inverse-in-einem-restklassenring/	   
+-- returns (gcd(a,b), (x, y)) in eq ax + by = gcd(a,b)
+euclid :: Integer -> Integer -> (Integer, (Integer, Integer))
+euclid 0 b = (b, (0, 1))
+euclid a b = 
+  (gcd, (y - (x*(b `div` a)), x))
+  where
+    (gcd, (x, y)) = euclid (b `mod` a) a
+
+eucl :: Integer -> Integer -> (Integer, Integer)
+eucl 0 b = (0,1)
+eucl a b
+  | b == 0 = (1,0)
+  | otherwise = (t, s-q*t)
+    where
+      (q, r) = quotRem a b
+      (s, t) = eucl b r
+
+inverseMod2 :: Integer -> Integer -> Integer
+inverseMod2 a m =
+  (x + m) `mod` m
+  where
+    (z, (x, y)) = ((gcd a m),eucl a m)
 
 -- use to save performance while doing millerRabin
 -- shiftR = bitwise right shift
 --squareMultiply b e = 
 
 -- public functions
-generatePrivateKey = True
-generatePublicKey = True
+generateKeyPair = True
+
 encrypt k n m = mod (m^k) n
 
 decrypt d n c = mod (c^d) n
