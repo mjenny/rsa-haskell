756c11b47cb0184b54498b03ecb1e2c035e7e5eb
Manuel Jenny <manuel.jenny@hispeed.ch>
2013-04-19 15:13:30 +0200

    Removed previous euclid and inverseMod

--- RSA.hs
+++ RSA.hs
@@ -66,37 +66,21 @@ isNotDivisor [] _ = True
 isNotDivisor (w:ws) x | (mod x w) == 0 = False
 		      | otherwise = isNotDivisor ws x
 		      
--- http://blog.agrafix.net/2012/08/haskell-multiplikatives-inverse-in-einem-restklassenring/	
--- multiplicative inverse for a in Z \ bZ
-inverseMod :: Integer -> Integer -> Integer
-inverseMod a m =
-  (x + m) `mod` m
-  where
-    (gcd, (x, y)) = euclid a m
 
--- http://blog.agrafix.net/2012/08/haskell-multiplikatives-inverse-in-einem-restklassenring/	   
--- returns (gcd(a,b), (x, y)) in eq ax + by = gcd(a,b)
-euclid :: Integer -> Integer -> (Integer, (Integer, Integer))
-euclid 0 b = (b, (0, 1))
-euclid a b = 
-  (gcd, (y - (x*(b `div` a)), x))
-  where
-    (gcd, (x, y)) = euclid (b `mod` a) a
-
-eucl :: Integer -> Integer -> (Integer, Integer)
-eucl 0 b = (0,1)
-eucl a b
+euclid :: Integer -> Integer -> (Integer, Integer)
+euclid 0 b = (0,1)
+euclid a b
   | b == 0 = (1,0)
   | otherwise = (t, s-q*t)
     where
       (q, r) = quotRem a b
-      (s, t) = eucl b r
+      (s, t) = euclid b r
 
-inverseMod2 :: Integer -> Integer -> Integer
-inverseMod2 a m =
+inverseMod :: Integer -> Integer -> Integer
+inverseMod a m =
   (x + m) `mod` m
   where
-    (z, (x, y)) = ((gcd a m),eucl a m)
+    (z, (x, y)) = ((gcd a m),euclid a m)
 
 -- use to save performance while doing millerRabin
 -- shiftR = bitwise right shift
