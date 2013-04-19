502b9961f93a4848b726996b4da814084818d6c4
Manuel Jenny <manuel.jenny@hispeed.ch>
2013-04-19 12:39:46 +0200

    redesigned isPrime function, added isNotDivisor and getDivisorList functions

--- RSA.hs
+++ RSA.hs
@@ -52,10 +52,20 @@ millerRabin x = (mod (2^(x-1)) x) not (1)
 isPrime :: Integer -> Bool
 isPrime 1 = False
 isPrime 2 = True
-isPrime x = isNotPrime x
-    where
-        isNotPrime z | even z == False && (not ((mod z 5) == 0)) && (not ((mod z 3) == 0)) && (not ((mod z 7) == 0)) = length [y | y <- 2:filter (not.even) [2.. (round (sqrt (fromInteger x)))], mod x y == 0] == 0
-                     | otherwise = False
+isPrime x | even x == False && (not ((mod x 5) == 0)) && (not ((mod x 3) == 0)) && (not ((mod x 7) == 0)) = isNotDivisor (getDivisorList x) x
+	  | otherwise = False
+
+-- returns a list of possible divisors for x
+getDivisorList :: Integer -> [Integer]                     
+getDivisorList x = [y | y <- 2:filter (not.even) [2.. (round (sqrt (fromInteger x)))]]
+
+-- checks wether there is an divisor or not
+-- returns False as soon as one divisor is found
+-- returns True if no divisor is found
+isNotDivisor :: [Integer] -> Integer -> Bool
+isNotDivisor [] _ = True
+isNotDivisor (w:ws) x | (mod x w) == 0 = False
+		      | otherwise = isNotDivisor ws x
 
 -- use to save performance while doing millerRabin
 -- shiftR = bitwise right shift
